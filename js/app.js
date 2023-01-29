20e15d9e6e4d0cf684c37261536cbd112d4c37c6
CristianoMends <mendescristiano012@gmail.com>
2023-01-28 23:29:55 -0300

    adicionado mais campos e imagens

--- js/app.js
+++ js/app.js
@@ -1,4 +1,30 @@
-var n1,n2,n3;
-n1 = 10
-n2 = 10; n3 =1;
-window.print(n1+n2) 
+var horarios = new Array(7);
+horarios[0] = "8:00";
+horarios[1] = "9:00";
+horarios[2] = "10:00";
+horarios[3] = "13:00";
+horarios[4] = "14:00";
+horarios[5] = "15:00";
+horarios[6] = "16:00";
+   
+    function drawImage(){
+        let select = document.querySelector("#services");
+        let optionIndex = select[select.selectedIndex];
+            if(optionIndex.value === "Corte"){            
+            document.getElementById("image").innerHTML 
+            = "<img class='imageFormat' src='src/images/corte.png'>";
+         }}
+
+    function time(){
+        let select = document.querySelector("#horarios");
+       for (let i = 0; i < 5; i++) {
+        console.log(horarios[0])
+            select.options[select.options.length] = new Option(horarios[i],"teste")            
+        }}    
+
+    function write1(){
+    window.alert("Agendamento concluído com sucesso");
+}
+
+
+window.onload = time
