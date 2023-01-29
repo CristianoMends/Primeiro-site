f14b2c6c9ab6ad40598d1fb8b9cebb6407aed5b4
CristianoMends <mendescristiano012@gmail.com>
2023-01-29 13:49:43 -0300

    Adicionado imagem de fundo, tela de login

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-var horarios = new Array(7);
+
+var horarios = [];
 horarios[0] = "8:00";
 horarios[1] = "9:00";
 horarios[2] = "10:00";
@@ -6,6 +7,9 @@ horarios[3] = "13:00";
 horarios[4] = "14:00";
 horarios[5] = "15:00";
 horarios[6] = "16:00";
+
+
+var servicos = new Array()["Corte","Alisamento","Coloração","Hidracacao profunda","Cauterização"]
    
     function drawImage(){
         let select = document.querySelector("#services");
@@ -14,17 +18,18 @@ horarios[6] = "16:00";
             document.getElementById("image").innerHTML 
             = "<img class='imageFormat' src='src/images/corte.png'>";
          }}
-
     function time(){
         let select = document.querySelector("#horarios");
-       for (let i = 0; i < 5; i++) {
-        console.log(horarios[0])
-            select.options[select.options.length] = new Option(horarios[i],"teste")            
+       for (let i = 0; i < horarios.length; i++) {
+            select.options[select.options.length] = new Option(horarios[i],horarios[i])            
         }}    
 
     function write1(){
     window.alert("Agendamento concluído com sucesso");
-}
-
-
+    }
+    function enterIndex(){
+        window.location.href = "index.html"
+    }
+    
+    
 window.onload = time
